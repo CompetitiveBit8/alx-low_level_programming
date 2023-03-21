b25e0d7c3a9fc2f188e5430e067a8575edda5075
CompetitiveBit8 <babatundeksamuel@gmail.com>
2023-03-21 13:53:42 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include 'main.h'
+
+/**
+ * print_alphabet_x10 - prints 10 times tge alphabets in lower case
+ */
+void print_alphabet_x10(void)
+{
+	int count = 0;
+
+	while (count++ <= 9)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
+	}
