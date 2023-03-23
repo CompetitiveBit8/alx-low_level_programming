f8e3ad6f8eff2171927c839ab7cb09e8ac5b095c
CompetitiveBit8 <babatundeksamuel@gmail.com>
2023-03-23 16:30:18 -0700

    batch push

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * _isupper - finds uppercase character
+ * _isupper - uppercase letters
  * @c: character to check
- * Return: this returns 0 when true and 1 when false
+ *
+ * Return: 0 or 1
  */
-
 int _isupper(int c)
 {
-if (c > "A" && c < "Z")
-	return (1);
+if (c >= 'A' && c <= 'Z')
+return (1);
 else
-	return (0);
+return (0);
 }
