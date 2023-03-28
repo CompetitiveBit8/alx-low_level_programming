00a8f9bd80c60c367c4687f3182581681114becd
CompetitiveBit8 <babatundeksamuel@gmail.com>
2023-03-27 22:55:35 -0700

    eight to ten

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,6 +8,10 @@ void _puts(char *str);
 int _putchar(char c);
 void print_rev(char *s);
 void puts2(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+
 
 
 #endif
