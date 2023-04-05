36a1389e5cffd48b01ac79a410a7e4ef4f55b297
CompetitiveBit8 <babatundeksamuel@gmail.com>
2023-04-05 12:45:33 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	char *var;
+
+	while(*haystach != '\0')
+	{
+		if(*haystack == ' ')
+			*var = *haystack +  1;
+	}
+	return(var);
+}		
